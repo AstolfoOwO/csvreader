a7bcc3366ddb296710512bd9924b49db93998f22
AstolfoOwO <Danilvasilkov59@gmail.com>
2024-05-29 02:26:57 +0700

    build project

---
+++ build/CMakeFiles/cvsreader.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mozzarella/csvreader/src/csv.cpp" "CMakeFiles/cvsreader.dir/src/csv.cpp.o" "gcc" "CMakeFiles/cvsreader.dir/src/csv.cpp.o.d"
+  "/home/mozzarella/csvreader/src/main.cpp" "CMakeFiles/cvsreader.dir/src/main.cpp.o" "gcc" "CMakeFiles/cvsreader.dir/src/main.cpp.o.d"
+  "/home/mozzarella/csvreader/src/tools.cpp" "CMakeFiles/cvsreader.dir/src/tools.cpp.o" "gcc" "CMakeFiles/cvsreader.dir/src/tools.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cvsreader.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvsreader.dir/src/csv.cpp.o"
+  "CMakeFiles/cvsreader.dir/src/csv.cpp.o.d"
+  "CMakeFiles/cvsreader.dir/src/main.cpp.o"
+  "CMakeFiles/cvsreader.dir/src/main.cpp.o.d"
+  "CMakeFiles/cvsreader.dir/src/tools.cpp.o"
+  "CMakeFiles/cvsreader.dir/src/tools.cpp.o.d"
+  "cvsreader"
+  "cvsreader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvsreader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
